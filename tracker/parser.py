d209e756b5f82f25c27b7684dc96a5d9a067f2a1
Arthur Laurent <laurent.arthur75@gmail.com>
2021-05-16 11:40:53 +0200

    Add a parser handler

---
+++ tracker/parser.py
@@ -0,0 +1,23 @@
+import argcomplete
+import argparse
+
+from tracker.models import Key
+
+
+def KeyCompleter(prefix=None, **kwargs):
+    return [key.name for key in Key.list_prefix(prefix)]
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("key", nargs="?").completer = KeyCompleter
+    parser.add_argument("value", nargs="?")
+    parser.add_argument("--timestamp", action="store", type=int)
+    parser.add_argument("--incr", "-i", action="store_true")
+    parser.add_argument("--day", "-t", action="store_true")
+    parser.add_argument("--describe", "-e", action="store_true")
+    parser.add_argument("--delete", "-d", action="store_true")
+    parser.add_argument("--delete-all", action="store_true")
+    argcomplete.autocomplete(parser)
+
+    return parser.parse_args()
